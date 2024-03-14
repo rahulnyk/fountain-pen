fa269d5a641a449ad6f862c84c4e3da71a2a0ac6
Nayak <rahul.nyk@gmail.com>
2024-03-14 18:57:53 +0530

    minor

--- src/app/components/fpr_editor/typography.tsx
+++ src/app/components/fpr_editor/typography.tsx
@@ -29,14 +29,12 @@ export const h3 = Spectral({
 });
 
 export const notes = Inconsolata({
-    weight: '300',
-    subsets: ['latin']
-})
+    weight: "300",
+    subsets: ["latin"],
+});
 
-
-export const 
-    paragraphStyle = `${p.className} text-xl pb-2`,
-    heading1Style = `${h1.className} text-4xl pt-10 pb-5`,
-    heading2Style = `${h2.className} text-3xl pt-10 pb-5`,
-    heading3Style = `${h3.className} text-2xl pt-10 pb-2`,
-    titleStyle = `${title.className} text-6xl pt-10 pb-5`;
+export const paragraphStyle = `${p.className} text-xl my-2`,
+    titleStyle = `${title.className} text-6xl my-2 pt-6 pb-0`,
+    heading1Style = `${h1.className} text-4xl my-2 pt-6 pb-0`,
+    heading2Style = `${h2.className} text-3xl my-2 pt-6 pb-0`,
+    heading3Style = `${h3.className} text-2xl my-2 pt-6 pb-0`;
