818c6af9b13133d69649f705503b124f0dcdd221
Nayak <rahul.nyk@gmail.com>
2024-08-05 17:38:46 +0530

    cleanup

--- src/app/_actions/llms/llm-gateway/utils.ts
+++ src/app/_actions/llms/llm-gateway/utils.ts
@@ -1,41 +1,18 @@
-import crypto from 'crypto';
-import { readEnvProperty } from '../../helpers/read_env_properties';
+import crypto from "crypto";
+import { readEnvProperty } from "../../helpers/read_env_properties";
 
 export interface ServiceRegistrySignature {
-  signature: string,
-  timestamp: string
+    signature: string;
+    timestamp: string;
 }
-
-
-export const signatureGenerator = (wnConsumerId: string, privateKey: string, keyVersion: string): ServiceRegistrySignature => {
-  const epoch_time_ms = Date.now();
-  const timestampStr: string = `${epoch_time_ms}`
-  const data = `${wnConsumerId}\n${epoch_time_ms}\n${keyVersion}\n`;
-  const signer = crypto.createSign('RSA-SHA256');
-  signer.update(data);
-  const signature = signer.sign(privateKey, 'base64');
-
-  return {
-    signature: signature,
-    timestamp: timestampStr
-  };
-};
 
 export function getHeaders() {
-  const headers = { 'x-api-key': readEnvProperty("WALMART_API_KEY", true) }
-  // const gatewayBaseUrl = readEnvProperty("WALMART_GATEWAY_BASEURL", true);
+    const headers = { "x-api-key": readEnvProperty("ORG_API_KEY", true) };
 
-  if (!headers['x-api-key']) {
-    throw new Error('Missing required environment variables WALMART_API_KEY. This is required for request header.')
-    // console.error('Missing required environment variables WALMART_API_KEY. This is required for request header.');
-    // return [];
-  }
-  return headers;
+    if (!headers["x-api-key"]) {
+        throw new Error(
+            "Missing required environment variables ORG_API_KEY. This is required for request header."
+        );
+    }
+    return headers;
 }
-
-// export function readEnvProperty(propertyKey: string, required: boolean): string {
-//   if (process.env[propertyKey] == undefined && required) {
-//     throw new Error("Please add " + propertyKey + " in your .env.local file");
-//   }
-//   return process.env[propertyKey] ? process.env[propertyKey] as string : "";
-// }
